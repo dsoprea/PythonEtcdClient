e31e490eb1a707c88df60535b4683c2e2028fa25
Dustin Oprea <myselfasunder@gmail.com>
2014-01-06 00:43:23 -0500

    Doc changes.

--- etcd/common_ops.py
+++ etcd/common_ops.py
@@ -1,9 +1,25 @@
 class CommonOps(object):
+    """Base-class of 'ops' modules."""
+
     def validate_path(self, path):
+        """Validate the key that we were given.
+
+        :param path: Key
+        :type path: string
+
+        :raises: ValueError
+        """
+
         if path[0] != '/':
             raise ValueError("Path [%s] should've been absolute." % (path))
 
     def get_fq_node_path(self, path):
+        """Return the full path of the given key.
+
+        :param path: Key
+        :type path: string
+        """
+
         self.validate_path(path)
 
         return ('/keys' + path)
--- etcd/config.py
+++ etcd/config.py
@@ -1,3 +1,8 @@
+"Default hostname for server."
 DEFAULT_HOSTNAME = '127.0.0.1'
+
+"Default port for server."
 DEFAULT_PORT = 4001
+
+"Default scheme for server."
 DEFAULT_SCHEME = 'http'
