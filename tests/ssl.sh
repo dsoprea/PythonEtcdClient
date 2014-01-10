e6f7ba4cc5795925b68eb3827e689c01723eef1d
Dustin Oprea <myselfasunder@gmail.com>
2014-01-10 13:31:26 -0500

    Added documentation for wait().

--- tests/ssl.sh
+++ tests/ssl.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-PEC_DEBUG=1 PEC_SCHEME=https PEC_HOSTNAME=etcd.local PEC_SSL_CA_BUNDLE_FILEPATH=ssl/rootCA.pem ./test.sh
+PEC_DEBUG=1 PEC_SCHEME=https PEC_HOSTNAME=etcd.local PEC_SSL_CA_BUNDLE_FILEPATH=ssl/demoCA/cacert.pem ./test.sh
 
--- tests/ssl/build_ssl_client.sh
+++ tests/ssl/build_ssl_client.sh
@@ -5,4 +5,4 @@ openssl genrsa -out client.key 2048
 openssl req -new -key client.key -out client.csr
 
 # Sign server certs.
-openssl x509 -req -in client.csr -CA rootCA.pem -CAkey rootCA.key -CAcreateserial -out client.crt -days 500
+openssl x509 -extensions ssl_client -req -in client.csr -CA rootCA.pem -CAkey rootCA.key -CAcreateserial -out client.crt -days 500
