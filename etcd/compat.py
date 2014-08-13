cff2dce60b167e43c4ec0126fab9ff86d2cc150f
Dustin Oprea <myselfasunder@gmail.com>
2014-08-13 13:47:12 -0400

    Bugfix in Python compatibility.

--- etcd/compat.py
+++ etcd/compat.py
@@ -1,4 +1,5 @@
 try:
-    from urlparse import parse_qsl, urlencode
+    from urlparse import parse_qsl
+    from urllib import urlencode
 except ImportError:
     from urllib.parse import parse_qsl, urlencode
